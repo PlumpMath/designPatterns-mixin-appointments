f5bdbeb925cd526f6daf773ceb36d855061e24e5
Veljko Knezevic <veljko.knezevic@gmail.com>
2016-12-21 17:43:52 -0500

    initialCommit

---
+++ Meeting.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Appointments
+{
+    class Meeting : IAppointment
+    {
+        private DateTime startTime;
+        private TimeSpan duration;
+
+        public Meeting(DateTime startTime, TimeSpan duration)
+        {
+
+        }
+    }
+}
